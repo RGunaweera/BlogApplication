dce811ca141c6ef1c05400f4450cd748af024803
RGunaweera <randima.g2002@gmail.com>
2024-08-14 12:52:21 +0530

    Created Sign In and Sign Up buttons

--- blog-app-frontend/src/App.js
+++ blog-app-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import User from './components/User';
 import './App.css';
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
     <Router>      
       <Routes>
         <Route path="/" element={<Home />} />
-        
+        <Route path="/signup" element={<User />} />
       </Routes>
     </Router>
   );
